734a0c1ab8b8b081c87e91e70c63eaa178d7fcd4
KaiJeng <darrenjeng15@gmail.com>
2016-12-15 21:30:12 -0500

    Updated data and app

--- code/app.R
+++ code/app.R
@@ -3,22 +3,18 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
+library(DT)
 
-# Initialize and some extra tidyings
+# Initialize
 Score_Sales_All <- read.csv("~/Documents/MSDS597/MSDS Project/data/Score_Sales_All", stringsAsFactors=FALSE)
 Score_Sales_All$X <- NULL
 Score_Sales_All$Week.Ending <- as.Date(Score_Sales_All$Week.Ending)
 Score_Sales_Week_One <- read.csv("~/Documents/MSDS597/MSDS Project/data/Score_Sales_Week_One", stringsAsFactors=FALSE)
 Score_Sales_Week_One$X <- NULL
 Score_Sales_Week_One$Week.Ending <- as.Date(Score_Sales_Week_One$Week.Ending)
-Score_Sales_Week_One$Genre[is.na(Score_Sales_Week_One$Genre)] <- "Unlisted"
-# Quick fix to be patched later
-for (i in 1:nrow(Score_Sales_Week_One)) {
-  if (is.na(Score_Sales_Week_One$Total[i] == FALSE)) {Score_Sales_Week_One$Complicated[i] <- "Sales data below:"}
-  if (is.na(Score_Sales_Week_One$Total[i] == TRUE)) {Score_Sales_Week_One$Complicated[i] <- "Sales data unavailable for this title."}
-}
 
-
+# Data set for interactive plot
+Score_Sales_Interactive <- Score_Sales_Week_One %>% filter(Complicated == "Sales data below:", Weekly != 0)
 
 # Building the UI
 ui <- fluidPage(
@@ -45,18 +41,18 @@ ui <- fluidPage(
                            dataTableOutput("tableNum"), "*Weekly sales numbers with value zero signify that data is unavailable",
                            tableOutput("dumdum"),
                            "Data provided by VGChartz.com"),
-                  tabPanel("Comparison: Sales", 
-                           fluidRow(column(4,selectInput("genes", "Genre", c("All", unique(as.character(Score_Sales_Week_One$Genre))))),
-                           column(4,selectInput("ploop", "Platform", c("All", unique(as.character(Score_Sales_Week_One$Platform)))))),
-                           dataTableOutput("table2"), "*First week sales",
-                           tableOutput("dummy"),
-                           "Data provided by VGChartz.com"),
-                  tabPanel("Comparison: Reviews", 
+                  tabPanel("Comparison: Sales and Reviews", 
                            fluidRow(column(4,selectInput("genes2", "Genre", c("All", unique(as.character(Score_Sales_Week_One$Genre))))),
                             column(4,selectInput("ploop2", "Platform", c("All", unique(as.character(Score_Sales_Week_One$Platform))))),
                             column(4,sliderInput("goalie", "Score Range", min = 0, max = 100, value = c(0, 100)))),
                            dataTableOutput("table3"),
-                           "Data provided by GameRankings.com")
+                           "*First week sales", tableOutput("dummbu"),
+                           "Data provided by VGChartz.com and GameRankings.com"),
+                  tabPanel("Comparison: Sales vs. Reviews",
+                           "Based on available data",
+                           plotOutput("plot4"), "Click on observation to highlight points on graph", dataTableOutput("table4"),
+                           "*First week sales", tableOutput("dummbuu"),
+                           "Data provided by VGChartz.com and GameRankings.com")
                   )
     )
   )
@@ -74,10 +70,11 @@ server <- function(input, output, session) {
                    "optionVITA" = "VITA",
                    "optionPC" = "PC"
     )
-    filter(Score_Sales_Week_One, Game == input$gameInput, Platform %in% plat)
+    
+    filter(Score_Sales_Week_One, Title == toupper(input$gameInput), Platform %in% plat)
   })
   output$tableDesc <- renderTable({ 
-    select(info_df(), -Pos, -Weekly, -Total, -Week.Number, -Week.Ending, -Complicated)
+    select(info_df(), -Pos, -Weekly, -Total, -Week.Number, -Week.Ending, -Complicated, -Title)
   })
   
   # Sales Numbers tab
@@ -90,11 +87,11 @@ server <- function(input, output, session) {
                   "optionVITA" = "VITA",
                   "optionPC" = "PC"
     )
-      filter(Score_Sales_All, Game == input$gameInput, Platform %in% plat)
+      filter(Score_Sales_All, Title == toupper(input$gameInput), Platform %in% plat)
   })
   output$whoops <- renderText ({
-       ies <- filter(Score_Sales_Week_One, Game == input$gameInput)
-       ies[[13]][1] 
+       ies <- filter(Score_Sales_Week_One, Title == toupper(input$gameInput))
+       ies$Complicated[1] 
   })
   output$plotNum <- renderPlot({
     ggplot(data = sales_df(), aes(Week.Ending, Weekly)) + geom_point() + scale_x_date() + xlab("Date") + ylab("Sales Number")
@@ -102,22 +99,10 @@ server <- function(input, output, session) {
   output$tableNum <- renderDataTable({ 
     select(sales_df(),Pos,Weekly,Week.Number,Week.Ending)
   })
-
-  # Comparison: Sales tab 
-  output$table2 <- renderDataTable({
-    data <- select(Score_Sales_Week_One,Platform,Game,Publisher,Genre,Pos,Weekly,Week.Ending)
-    if (input$genes != "All") {
-      data <- data[data$Genre == input$genes,]
-    }
-    if (input$ploop != "All") {
-      data <- data[data$Platform == input$ploop,]
-    }
-    data
-  })
   
-  # Comparison: Reviews tab
+  # Comparison tab
   output$table3 <- renderDataTable({
-    data2 <- select(Score_Sales_Week_One,Platform,Game,Publisher,Genre,Score,Number.of.Reviews,Week.Ending)
+    data2 <- select(Score_Sales_Week_One,Platform,Game,Publisher,Genre,Score,Weekly)
     if (input$genes2 != "All") {
       data2 <- data2[data2$Genre == input$genes2,]
     }
@@ -126,6 +111,17 @@ server <- function(input, output, session) {
     }
     filter(data2, Score >= input$goalie[1] & Score <= input$goalie[2])
   })
+  
+  # Comparison vs. tab
+  ssint <- select(Score_Sales_Interactive,Score,Weekly)
+  output$table4 <- DT::renderDataTable(select(Score_Sales_Interactive,Platform,Game,Publisher,Genre,Score,Weekly,Week.Ending), server = FALSE)
+  output$plot4 <- renderPlot({
+    s = input$table4_rows_selected
+    par(mar = c(4, 4, 1, .1))
+    plot(ssint)
+    if (length(s)) points(ssint[s, , drop = FALSE], pch = 19, cex = 2)
+  })
+
   
 }
 shinyApp(ui = ui, server = server)
